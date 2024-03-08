56c91d688447af369c9d607b73235a73453f9b40
nathan <nathanmanzano@gmail.com>
2024-03-08 22:21:01 +0800

    branch add

--- app/src/main/java/com/example/pawandroid/IncomingRequestActivity.kt
+++ app/src/main/java/com/example/pawandroid/IncomingRequestActivity.kt
@@ -1,11 +1,14 @@
 package com.example.pawandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pawandroid.adapter.IncomingRequestAdapter
+import com.example.pawandroid.bottomNav.MainActivity
+import com.example.pawandroid.bottomNav.ProfileActivity
 import com.example.pawandroid.builder.RetrofitBuilder
 import com.example.pawandroid.databinding.ActivityIncomingRequestBinding
 import com.example.pawandroid.model.Adopt
@@ -17,15 +20,22 @@ import retrofit2.Response
 class IncomingRequestActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIncomingRequestBinding
     private lateinit var recyclerView: RecyclerView
+    private var btnBack : String? = null
     private lateinit var incomingRequestDetailAdapter: IncomingRequestAdapter
     private lateinit var adoptList: MutableList<Adopt>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIncomingRequestBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         init()
         incomingRequestList()
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra("key", btnBack)
+            startActivity(intent)
+
+        }
     }
 
     override fun onResume() {
